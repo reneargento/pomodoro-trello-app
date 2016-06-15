a5b4718467659312abcd3cd72cd0bc46e56d3344
Rene Argento <rene.argento@gmail.com>
2016-06-14 21:38:52 -0300

    Initial commit

--- PomodoroTrello/app/src/main/java/com/rene/pomodorotrello/activities/ConfigActivity.java
+++ PomodoroTrello/app/src/main/java/com/rene/pomodorotrello/activities/ConfigActivity.java
@@ -11,5 +11,6 @@ public class ConfigActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_config);
+        
     }
 }
